ac6addf4f6b2ea0853e260473db39c9f37ce2f04
Duane Cilliers <duane@signpost.co.za>
2015-11-17 16:59:42 +0200

    initial commit

---
+++ bb-social-widget.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Beaver Builder Social Widget
+ * Plugin URI: https://github.com/duanecilliers/BB-Social-Widget
+ * Description: WordPress social icons widget for social links added to customizer by Beaver Builder theme.
+ * Version: 1.0.0
+ * Author: Duane Cilliers
+ * Author URI: http://duane.co.za
+ * License: GNU General Public License v2.0
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: bb-social-widget
+ */
